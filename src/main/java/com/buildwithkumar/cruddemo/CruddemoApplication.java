eb4c0372029138ef3297b25d2acdf14c80e463be
Kumar Sagar <kumar.sagar.ks6778106@gmail.com>
2025-09-06 09:00:37 +0530

    create multiple entries in db using jpa

--- src/main/java/com/buildwithkumar/cruddemo/CruddemoApplication.java
+++ src/main/java/com/buildwithkumar/cruddemo/CruddemoApplication.java
@@ -17,8 +17,27 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner -> {
-			createStudent(studentDAO);
+//			createStudent(studentDAO);
+			createMultipleStudent(studentDAO);
 		};
+	}
+
+	private void createMultipleStudent(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Creating 3 new student object.....");
+		Student tempStudent1 = new Student("Paul", "Doe", "paul@buildwithkumar.com");
+		Student tempStudent2 = new Student("Mary", "Jane", "mary@buildwithkumar.com");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@buildwithkumar.com");
+
+		//save the student object
+		System.out.println("Saving the students....");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+		//display the id of the saved object
+		System.out.println("Saved student1. Generating id: " + tempStudent1.getId());
+		System.out.println("Saved student2. Generating id: " + tempStudent2.getId());
+		System.out.println("Saved student3. Generating id: " + tempStudent3.getId());
 	}
 
 	private void createStudent(StudentDAO studentDAO) {

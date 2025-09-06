918804ee9e451783f6be90f876ac0afd4faf3702
Kumar Sagar <kumar.sagar.ks6778106@gmail.com>
2025-09-06 08:43:44 +0530

    data object model and save an entry in db using jpa

--- src/main/java/com/buildwithkumar/cruddemo/CruddemoApplication.java
+++ src/main/java/com/buildwithkumar/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.buildwithkumar.cruddemo;
 
+import com.buildwithkumar.cruddemo.dao.StudentDAO;
+import com.buildwithkumar.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,23 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner -> {
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("Creating the new student object.....");
+		Student tempStudent = new Student("Paul", "Doe", "paul@buildwithkumar.com");
+
+		//save the student object
+		System.out.println("Saving the student....");
+		studentDAO.save(tempStudent);
+		//display the id of the saved object
+		System.out.println("Saved student. Generating id: " + tempStudent.getId());
 	}
 
 }
---
+++ src/main/java/com/buildwithkumar/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.buildwithkumar.cruddemo.dao;
+
+import com.buildwithkumar.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+}

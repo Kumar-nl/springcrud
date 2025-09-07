f7cf58172f8ae4debc2fc59a290c7328960170c5
Kumar Sagar <kumar.sagar.ks6778106@gmail.com>
2025-09-07 19:39:45 +0530

    crud operations and table creation from code using application.prop

--- src/main/java/com/buildwithkumar/cruddemo/CruddemoApplication.java
+++ src/main/java/com/buildwithkumar/cruddemo/CruddemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -19,7 +21,79 @@ public class CruddemoApplication {
 		return runner -> {
 //			createStudent(studentDAO);
 			createMultipleStudent(studentDAO);
+//			retrieveStudent(studentDAO);
+//			queryForStudents(studentDAO);
+//			queryForStudentsByLastName(studentDAO);
+//			updateStudent(studentDAO);
+//			deleteStudent(studentDAO);
+//			deleteAllStudents(studentDAO);
 		};
+	}
+
+	private void deleteAllStudents(StudentDAO studentDAO) {
+		System.out.println("Deleting all students...");
+		int numsRowsDeleted = studentDAO.deleteAll();
+		System.out.println("Deleted rows count: "+ numsRowsDeleted);
+	}
+
+	private void deleteStudent(StudentDAO studentDAO) {
+		int studentId = 3;
+		System.out.println("Deleting the student Id: " + studentId);
+		studentDAO.delete(studentId);
+	}
+
+	private void updateStudent(StudentDAO studentDAO) {
+
+		// retrieve student based on the id: primary key
+		int studentId = 1;
+		System.out.println("Getting student with id: " + studentId);
+		Student myStudent = studentDAO.findById(studentId);
+		//change first name to scobby
+		System.out.println("Updating student.......");
+		myStudent.setFirstName("Scooby");
+		//update the student
+		studentDAO.update(myStudent);
+		System.out.println("Updated student: "+ myStudent);
+	}
+
+	private void queryForStudentsByLastName(StudentDAO studentDAO) {
+
+		//get a list of students
+		List<Student> theStudents = studentDAO.findByLastName("Doe");
+
+		//display the list of students
+		for(Student tempStudent : theStudents){
+			System.out.println(tempStudent);
+		}
+	}
+
+	private void queryForStudents(StudentDAO studentDAO) {
+
+		//get a list of students
+		List<Student> theStudents = studentDAO.findAll();
+
+		//display the list of students
+		for(Student tempStudent : theStudents){
+			System.out.println(tempStudent);
+		}
+	}
+
+	private void retrieveStudent(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Creating the new student object.....");
+		Student tempStudent = new Student("John", "Doe", "john@buildwithkumar.com");
+
+		//save the student object
+		System.out.println("Saving the student....");
+		studentDAO.save(tempStudent);
+		//display the id of the saved object
+		int theId = tempStudent.getId();
+		System.out.println("Saved student. Generating id: " + theId);
+		//retrieve student based on that id
+		System.out.println("Retrieving the student with the given id: " + theId);
+		Student myStudent = studentDAO.findById(theId);
+		//display the student
+		System.out.println("Found the student: " + myStudent);
 	}
 
 	private void createMultipleStudent(StudentDAO studentDAO) {
--- src/main/java/com/buildwithkumar/cruddemo/dao/StudentDAO.java
+++ src/main/java/com/buildwithkumar/cruddemo/dao/StudentDAO.java
@@ -2,7 +2,21 @@ package com.buildwithkumar.cruddemo.dao;
 
 import com.buildwithkumar.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
+
+    void delete(Integer id);
+
+    int deleteAll();
 }
